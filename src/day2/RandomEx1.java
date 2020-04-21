69be319f0d5e4a84c45497310d42bae97476cd54
jin <lijvlij@naver.com>
2020-04-21 13:41:48 +0900

    4.20 day2 Random문 예제

---
+++ src/day2/RandomEx1.java
@@ -0,0 +1,25 @@
+package day2;
+
+public class RandomEx1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int min = 1;
+		int max = 45;
+		int random = 0;
+		random = (int)(Math.random()*(max-min+1))+min;
+		System.out.println(random);
+		random = (int)(Math.random()*(max-min+1))+min;
+		System.out.println(random);
+		random = (int)(Math.random()*(max-min+1))+min;
+		System.out.println(random);
+		random = (int)(Math.random()*(max-min+1))+min;
+		System.out.println(random);
+		random = (int)(Math.random()*(max-min+1))+min;
+		System.out.println(random);
+		random = (int)(Math.random()*(max-min+1))+min;
+		System.out.println(random);
+
+	}
+
+}
