5eae441737bdd312aa7f7f30f6de936ef40c0ae4
jin <lijvlij@naver.com>
2020-04-28 16:35:37 +0900

    4.28 day8 test 작용하는 순서

---
+++ src/day8/testEx1.java
@@ -0,0 +1,22 @@
+package day8;
+
+public class testEx1 {
+
+	public static void main(String[] args) {
+/* 5 7 8 15 16 8 9 14 순으로 동작함 (11~13이 주석인경우)
+		int num1=1,num2=2;
+		int res=sum(num1,num2);
+		System.out.println(res); */
+//5 11 12 18 19 15 16 19 12 13 14 (7~9줄이 주석인 경우)
+		int num1=1,num2=2;
+		int res=sum2(num1,num2,3);
+		System.out.println(res);
+	}
+	public static int sum(int num1, int num2) {
+		return num1 + num2;
+	}
+	public static int sum2(int num1,int num2, int num3) {
+		return sum(num1,num2)+num3;
+	}
+
+}
